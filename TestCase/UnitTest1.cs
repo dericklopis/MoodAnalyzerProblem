ac4ce6613a271c2ea297b9e68f9382c32c4df822
dericklopis <stanforever08@gmail.com>
2023-01-20 08:49:54 +0530

    Added:Exception

--- TestCase/UnitTest1.cs
+++ TestCase/UnitTest1.cs
@@ -6,9 +6,36 @@ namespace TestCase
         [TestMethod]
         public void TestHappyOrSad()
         {
-            MoodAnalyzerProblem.MoodAnalyzer objMood = new MoodAnalyzerProblem.MoodAnalyzer(); // Arrange
+            MoodAnalyzerProblem.MoodAnalyzer objMood = new MoodAnalyzerProblem.MoodAnalyzer("Happy"); // Arrange
 
-            string result = objMood.AnalyzeMood("Happy"); // Act
+            string result = objMood.AnalyzeMood(); // Act
+
+            Assert.AreEqual("Happy".ToUpper(), result); //Assert
+        }
+        [TestMethod]
+        public void GivenSad_ReturnSad()
+        {
+            MoodAnalyzerProblem.MoodAnalyzer objMood = new MoodAnalyzerProblem.MoodAnalyzer("I am in Sad Mood"); // Arrange
+
+            string result = objMood.AnalyzeMood(); // Act
+
+            Assert.AreEqual("Sad".ToUpper(), result); //Assert
+        }
+        [TestMethod]
+        public void GivenAny_ReturnHappy()
+        {
+            MoodAnalyzerProblem.MoodAnalyzer objMood = new MoodAnalyzerProblem.MoodAnalyzer("I am in Any Mood"); // Arrange
+
+            string result = objMood.AnalyzeMood(); // Act
+
+            Assert.AreEqual("Happy".ToUpper(), result); //Assert
+        }
+        [TestMethod]
+        public void GivenNull_ReturnHappy()
+        {
+            MoodAnalyzerProblem.MoodAnalyzer objMood = new MoodAnalyzerProblem.MoodAnalyzer(null); // Arrange
+
+            string result = objMood.AnalyzeMood(); // Act
 
             Assert.AreEqual("Happy".ToUpper(), result); //Assert
         }
