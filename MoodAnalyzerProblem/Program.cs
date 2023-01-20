39e966397889c66f13867d0caa52b7e8206bf32f
dericklopis <stanforever08@gmail.com>
2023-01-20 08:43:41 +0530

    Added:Respond Happy or sad

--- MoodAnalyzerProblem/Program.cs
+++ MoodAnalyzerProblem/Program.cs
@@ -6,11 +6,33 @@ using System.Threading.Tasks;
 
 namespace MoodAnalyzerProblem
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Mood Analyzer Program");
+        }
+    }
+
+    public class MoodAnalyzer
+    {
+        public string AnalyzeMood(string message)
+        {
+            var mood1 = message.Contains("Happy", StringComparison.OrdinalIgnoreCase);
+            if (mood1)
+            {
+                return "HAPPY";
+            }
+            var mood2 = message.Contains("Sad", StringComparison.OrdinalIgnoreCase);
+            if (mood2)
+            {
+                return "SAD";
+            }
+            else
+            {
+                return "HAPPY";
+            }
+            //return String.Empty;
         }
     }
 }
